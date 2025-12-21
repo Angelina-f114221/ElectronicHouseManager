b825768baad38fc11092df95670ab7df1a1c0730
Angelina Ivanova <angelina@gmail.com>
2025-12-22 00:26:54 +0200

    Creating entity class Resident

---
+++ src/main/java/org/example/entity/Resident.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Resident {
+    @Id
+    private long id;
+    private String name;
+    private int age;
+    private boolean uses_elevator;
+    private boolean pet_uses_common_areas;
+    private LocalDate contract_start;
+    private LocalDate last_payment;
+    private boolean has_paid_monthly_fee;
+    private boolean is_owner;
+    // apartment_id INT
+}
