2a626e547cf3f279edc9d31f0b8a71debd60bf0c
Angelina Ivanova <angelina@gmail.com>
2025-12-22 00:25:04 +0200

    Creating entity class Building

---
+++ src/main/java/org/example/entity/Building.java
@@ -0,0 +1,19 @@
+package org.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Building {
+    @Id
+    private long id;
+    private String name;
+    private int floors;
+    private String address;
+    private double common_areas;
+    private double total_areas;
+    private int n_apartments;
+    private double fee_per_sqm;
+    private double fee_per_pet_using_ca;
+    // employee_id INT
+}
