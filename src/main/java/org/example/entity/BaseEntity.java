6a4b4c9dfe72b99f6a0a50d52a1fa410573a8401
Angelina Ivanova <angelina@gmail.com>
2025-12-23 15:53:44 +0200

    Adding getter, setter and toString annotations to BaseEntity class

--- src/main/java/org/example/entity/BaseEntity.java
+++ src/main/java/org/example/entity/BaseEntity.java
@@ -1,9 +1,16 @@
 package org.example.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /* абстракция, която служи за стратегия за генериране; този клас не е entity, защото няма да има таблица за него. другите класове няма да имат описани в тях първични ключове, защото ще екстендват този клас с неговите анотации.*/
 @MappedSuperclass
+// constructors
+@Getter
+@Setter
+@ToString
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
