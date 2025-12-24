18e16e9e75cbfbeec96ffe7c0f219dd61ed5ad5e
Angelina Ivanova <angelina@gmail.com>
2025-12-25 00:22:04 +0200

    annotating the relationships of class Company

--- src/main/java/org/example/entity/Company.java
+++ src/main/java/org/example/entity/Company.java
@@ -1,9 +1,8 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /*
 представлява ентити клас, който ще отговаря за таблица в базата и за моделите на данните ни. За да може класа компания да отговаря за таблица, трябва да бъде означен с entity анотация. ентити моделът служи за определяне на структурата на таблицата и за манипулиране на данните. първичният ключ трябва да се анотира изрично, иначе ще има грешка. тази entity анотация идва от jakarta и persistence подмодула. това е технологичен компонент, който осигурява да се ползват анотации за връзката между обектния и релационния модел и след това Hibernate работи с тях.
@@ -13,4 +12,10 @@ public class Company extends BaseEntity {
     // анотирам ID-то, което също е от Jakarta
     // При генериране на записи, id-тата на една таблица са независими от тези на друга. позволява ни да имаме auto increment при code first подхода.
    private String name;
+
+   @OneToMany(mappedBy = "company")
+    private Set<Building> buildings;
+
+   @OneToMany(mappedBy = "company")
+    private Set<Employee> employees;
 }
