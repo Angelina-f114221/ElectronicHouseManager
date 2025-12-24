444c8bee1153a194bf940b05745b5ffe7ff4e85d
Angelina Ivanova <angelina@gmail.com>
2025-12-25 00:28:24 +0200

    annotating the relationships of class Owner

--- src/main/java/org/example/entity/Owner.java
+++ src/main/java/org/example/entity/Owner.java
@@ -1,13 +1,15 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Owner extends BaseEntity {
     private String name;
-    // apartment_id INT
+    // ???
+    @ManyToMany
+    private Set<Apartment> apartments;
+
     // resident_id INT
 }
