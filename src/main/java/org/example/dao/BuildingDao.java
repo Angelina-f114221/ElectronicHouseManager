4568fb53cb81d47e97d0e04b9abec3d4901b8817
Angelina Ivanova <angelina@gmail.com>
2025-12-26 01:52:52 +0200

    Adding method updateBuilding in class BuildingDao

--- src/main/java/org/example/dao/BuildingDao.java
+++ src/main/java/org/example/dao/BuildingDao.java
@@ -27,4 +27,20 @@ public class BuildingDao {
             return session.find(Building.class, id);
         }
     }
+    public static void updateBuilding(long id, Building building) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Building building1 = session.find(Building.class, id);
+            // ??? - name
+            building1.setName(building.getName());
+            building1.setAddress(building.getAddress());
+            building1.setCommon_areas(building.getCommon_areas());
+            building1.setFee_per_pet_using_ca(building.getFee_per_pet_using_ca());
+            building1.setFee_per_sqm(building.getFee_per_sqm());
+            building1.setN_apartments(building.getN_apartments());
+            building1.setTotal_areas(building.getTotal_areas());
+            session.persist(building1);
+            transaction.commit();
+        }
+    }
 }
