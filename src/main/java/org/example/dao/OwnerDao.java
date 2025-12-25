02b8345244df069a906054c45c41e459aef90016
Angelina Ivanova <angelina@gmail.com>
2025-12-26 01:24:28 +0200

    Create method getOwners in class OwnerDao

--- src/main/java/org/example/dao/OwnerDao.java
+++ src/main/java/org/example/dao/OwnerDao.java
@@ -6,12 +6,20 @@ import org.example.entity.Owner;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class OwnerDao {
     public static void createOwner(Owner owner) {
         try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.persist(owner);
             transaction.commit();
+        }
+    }
+    public static List<Owner> getOwners() {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT o FROM Owner o", Owner.class)
+                    .getResultList();
         }
     }
 }
