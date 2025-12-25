4636e815a394a06a08ea2bee3df8aa7b957d071b
Angelina Ivanova <angelina@gmail.com>
2025-12-26 01:25:46 +0200

    Create method getResidents in class ResidentDao

--- src/main/java/org/example/dao/ResidentDao.java
+++ src/main/java/org/example/dao/ResidentDao.java
@@ -6,12 +6,20 @@ import org.example.entity.Resident;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class ResidentDao {
     public static void createResident(Resident resident) {
         try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.persist(resident);
             transaction.commit();
+        }
+    }
+    public static List<Resident> getResidents() {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT r FROM Resident r", Resident.class)
+                    .getResultList();
         }
     }
 }
