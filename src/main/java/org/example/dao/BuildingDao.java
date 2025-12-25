de61e44f8c70c875115baa349d7a94a0956d378a
Angelina Ivanova <angelina@gmail.com>
2025-12-26 01:22:05 +0200

    Create method getBuildings in class BuildingDao

--- src/main/java/org/example/dao/BuildingDao.java
+++ src/main/java/org/example/dao/BuildingDao.java
@@ -6,12 +6,20 @@ import org.example.entity.Company;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class BuildingDao {
     public static void createBuilding(Building building) {
         try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             session.persist(building);
             transaction.commit();
+        }
+    }
+    public static List<Building> getBuildings() {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.createQuery("SELECT b FROM Building b", Building.class)
+                    .getResultList();
         }
     }
 }
