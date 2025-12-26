a585c0d153cb9649a67abf39a85f267aa3ccdd69
Angelina Ivanova <angelina@gmail.com>
2025-12-27 00:32:32 +0200

    Adding method updateResident in class ResidentDao

--- src/main/java/org/example/dao/ResidentDao.java
+++ src/main/java/org/example/dao/ResidentDao.java
@@ -27,4 +27,20 @@ public class ResidentDao {
             return session.find(Resident.class, id);
         }
     }
+    public static void updateResident(long id, Resident resident) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Resident resident1 = session.find(Resident.class, id);
+            resident1.setName(resident.getName());
+            resident1.set_owner(resident.is_owner());
+            resident1.setAge(resident.getAge());
+            resident1.setUses_elevator(resident.isUses_elevator());
+            resident1.setContract_start(resident.getContract_start());
+            resident1.setHas_paid_monthly_fee(resident.isHas_paid_monthly_fee());
+            resident1.setLast_payment(resident.getLast_payment());
+            resident1.setPet_uses_common_areas(resident.isPet_uses_common_areas());
+            session.persist(resident1);
+            transaction.commit();
+        }
+    }
 }
